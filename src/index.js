20a8d933e7d4110c6d5b4da6e1fe73d1200439c4
Octavio <octavio@vercetti.vision>
2020-12-03 10:23:23 +0100

    git init

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+const typeDefs = `
+type Query {
+    info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
+}
+`;
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url,
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
