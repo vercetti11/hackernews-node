c5904744ed9604d72304d23be12323665a0ce995
Octavio <octavio@vercetti.vision>
2020-12-03 14:31:10 +0100

    Removed hardcoded links array and the id count since now storage will be persisted in a real database

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { PrismaClient } = require("@prisma/client");
 
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL",
-  },
-];
-
-let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
