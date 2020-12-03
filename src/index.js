10189163162e1af84a88f16edd788048daf3bc1b
Octavio <octavio@vercetti.vision>
2020-12-03 14:13:28 +0100

    Attached an instance of PrismaClient to the context when the GraphQLServer is beeing initialized

--- src/index.js
+++ src/index.js
@@ -42,8 +42,13 @@ const resolvers = {
   },
 };
 
+const prisma = new PrismaClient();
+
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
+  context: {
+    prisma,
+  },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
