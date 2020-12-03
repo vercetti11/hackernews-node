3f361406c666b6578f08f9ee6f0235982f7fc8a0
Octavio <octavio@vercetti.vision>
2020-12-03 10:51:27 +0100

    Implemented a simple mutation

--- src/index.js
+++ src/index.js
@@ -8,15 +8,22 @@ let links = [
   },
 ];
 
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url,
+  Mutation: {
+    post: (_, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
   },
 };
 
