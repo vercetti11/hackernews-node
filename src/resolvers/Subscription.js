ef300730aeabf82fdff5619bc55fb1bb82592282
Octavio <octavio@vercetti.vision>
2020-12-04 14:58:33 +0100

    New vote subscription implemented

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -9,6 +9,18 @@ const newLink = {
   },
 };
 
+function newVoteSubscribe(_, __, context) {
+  return context.pubsub.asyncIterator("NEW_VOTE");
+}
+
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => {
+    return payload;
+  },
+};
+
 module.exports = {
   newLink,
+  newVote,
 };
